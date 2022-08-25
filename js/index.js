bc4c67e068c3f66d29aebc6353ee33a2f30fbf16
Christopher Guevarra <cjg73902@gmail.com>
2022-08-25 17:23:03 -0400

    Create unit test for determineWinner function

--- js/index.js
+++ js/index.js
@@ -30,21 +30,40 @@ function determineWinner(player1, player2) {
   } else {
     switch (player1) {
       case "Rock": {
-        if (player2 === "Scissors") {
+        if (player2 == "Scissors") {
           return "You Win!";
-        }
+        }break;
       }
       case "Scissors": {
         if (player2 === "Paper") {
           return "You Win!";
-        }
+        }break;
       }
       case "Paper": {
         if (player2 === "Rock") {
           return "You Win!";
-        }
+        }break;
       }
-      default:{return "You Lose!"}
+      default:return "Error in determine Winner"
     }
+    return "You Lose!";
   }
+}
+
+function determineWinnerTest(){
+  //Test Rock & Scissors 
+  let testResult1 = determineWinner("Rock","Scissors");
+  testResult1==="You Win!" ? console.log("Rock & Scissor test PASSED"):console.log("Rock & Scissor test FAILED");
+  //Test Scissors & Paper
+  let testResult2 = determineWinner("Scissors","Paper");
+  testResult2==="You Win!" ? console.log("Scissors & Paper test PASSED"):console.log("Scissors & Paper test FAILED");
+  //Test Paper & Rock
+  let testResult3 = determineWinner("Paper","Rock");
+  testResult3==="You Win!" ? console.log("Paper & Rock test PASSED"):console.log("Paper & Rock test FAILED");
+  //Test Similar Choice
+  let testResult4 = determineWinner("Rock","Rock");
+  testResult4 ==="Tie" ? console.log("Same choice test PASSED"):console.log("Same choice test FAILED");
+  //Test Lose condition Scissors & Rock
+  let testResult5 = determineWinner("Scissors","Rock");
+  testResult5 === "You Lose!" ? console.log("Lose condition test PASSED"):console.log("Lose condition test FAILED");
 }
