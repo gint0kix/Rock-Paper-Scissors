4a0027a5343628eea98b03ba3ecde0e1be2b0d05
Christopher Guevarra <cjg73902@gmail.com>
2022-08-26 18:57:03 -0400

    Create game function with win counter

--- js/index.js
+++ js/index.js
@@ -31,61 +31,120 @@ function determineWinner(player1, player2) {
     switch (player1) {
       case "Rock": {
         if (player2 == "Scissors") {
-          return "You Win!";
-        }break;
+          return "Win";
+        }
+        break;
       }
       case "Scissors": {
         if (player2 === "Paper") {
-          return "You Win!";
-        }break;
+          return "Win";
+        }
+        break;
       }
       case "Paper": {
         if (player2 === "Rock") {
-          return "You Win!";
-        }break;
+          return "Win";
+        }
+        break;
       }
-      default:return "Error in determine Winner"
+      default:
+        return "Error in determine Winner";
     }
-    return "You Lose!";
+    return "Lose";
   }
 }
 
-function determineWinnerTest(){
-  //Test Rock & Scissors 
-  let testResult1 = determineWinner("Rock","Scissors");
-  testResult1==="You Win!" ? console.log("Rock & Scissor test PASSED"):console.log("Rock & Scissor test FAILED");
+function determineWinnerTest() {
+  //Test Rock & Scissors
+  let testResult1 = determineWinner("Rock", "Scissors");
+  testResult1 === "Win"
+    ? console.log("Rock & Scissor test PASSED")
+    : console.log("Rock & Scissor test FAILED");
   //Test Scissors & Paper
-  let testResult2 = determineWinner("Scissors","Paper");
-  testResult2==="You Win!" ? console.log("Scissors & Paper test PASSED"):console.log("Scissors & Paper test FAILED");
+  let testResult2 = determineWinner("Scissors", "Paper");
+  testResult2 === "Win"
+    ? console.log("Scissors & Paper test PASSED")
+    : console.log("Scissors & Paper test FAILED");
   //Test Paper & Rock
-  let testResult3 = determineWinner("Paper","Rock");
-  testResult3==="You Win!" ? console.log("Paper & Rock test PASSED"):console.log("Paper & Rock test FAILED");
+  let testResult3 = determineWinner("Paper", "Rock");
+  testResult3 === "Win"
+    ? console.log("Paper & Rock test PASSED")
+    : console.log("Paper & Rock test FAILED");
   //Test Similar Choice
-  let testResult4 = determineWinner("Rock","Rock");
-  testResult4 ==="Tie" ? console.log("Same choice test PASSED"):console.log("Same choice test FAILED");
+  let testResult4 = determineWinner("Rock", "Rock");
+  testResult4 === "Tie"
+    ? console.log("Same choice test PASSED")
+    : console.log("Same choice test FAILED");
   //Test Lose condition Scissors & Rock
-  let testResult5 = determineWinner("Scissors","Rock");
-  testResult5 === "You Lose!" ? console.log("Lose condition test PASSED"):console.log("Lose condition test FAILED");
+  let testResult5 = determineWinner("Scissors", "Rock");
+  testResult5 === "Lose"
+    ? console.log("Lose condition test PASSED")
+    : console.log("Lose condition test FAILED");
 }
 
-function formatUserInput(userInput){
+function formatUserInput(userInput) {
   let formatedInput = userInput.toLowerCase();
-  formatedInput = formatedInput.substring(0,1).toUpperCase() + formatedInput.substring(1);
+  formatedInput =
+    formatedInput.substring(0, 1).toUpperCase() + formatedInput.substring(1);
   return formatedInput;
 }
 
-function formatUserInputTest(){
+function formatUserInputTest() {
   //Testing all lowercase input
   let test1 = formatUserInput("abcd");
-  test1==="Abcd"?console.log("all lowerCase PASSED"):console.log("all lowerCase FAILED");
+  test1 === "Abcd"
+    ? console.log("all lowerCase PASSED")
+    : console.log("all lowerCase FAILED");
 
   //Testing all uppercase input
   let test2 = formatUserInput("ABCD");
-  test2==="Abcd"?console.log("all uppercasse PASSED"):console.log("all uppercase FAILED");
+  test2 === "Abcd"
+    ? console.log("all uppercasse PASSED")
+    : console.log("all uppercase FAILED");
 
   //Testintg random casing input
   let test3 = formatUserInput("AbCd");
-  test3==="Abcd"?console.log("random casing PASSED"):console.log("random casing FAILED");
+  test3 === "Abcd"
+    ? console.log("random casing PASSED")
+    : console.log("random casing FAILED");
 }
 
-formatUserInputTest();
+function isValidInput(userInput) {
+  if (
+    userInput === "Rock" ||
+    userInput === "Paper" ||
+    userInput === "Scissors"
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function playRound() {
+  let userInput = prompt("Please Enter Rock, Paper, or Scissors");
+  let formatedInput = formatUserInput(userInput);
+  if (isValidInput(formatedInput)) {
+    let computerChoice = getComputerChoice();
+    let result = determineWinner(formatedInput, computerChoice);
+    return {result:result,userChoice:formatedInput,computerChoice:computerChoice};
+  } else {
+    console.log("Invalid input, Please Enter either Rock, Paper, Scissors");
+    return {result:"Error",userChoice:formatedInput,computerChoice:computerChoice};
+  }
+}
+
+function game(numberOfGames = 5) {
+  let winCounter = 0;
+  for (let i = 0; i < numberOfGames; i++) {
+    let {result,userChoice,computerChoice} = playRound(); 
+    if (result == "Win") {
+      console.log(`You Won!, ${userChoice} beats ${computerChoice}`);
+      winCounter++;
+    } else if (result == "Lose") {
+      console.log(`You Lost, ${userChoice} beats ${computerChoice}`);
+    } else {
+      console.log(`${result}`);
+    }
+  }
+  console.log(`You won ${winCounter} times! \\(^.^)/`)
+}
